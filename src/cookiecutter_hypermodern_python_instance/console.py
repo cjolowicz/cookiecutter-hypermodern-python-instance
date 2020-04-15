b4b0e07f44d661c0c53bdfc97beb11c79d3f990d
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-04-15 07:06:15 +0200

    Add function without code coverage

--- src/cookiecutter_hypermodern_python_instance/console.py
+++ src/cookiecutter_hypermodern_python_instance/console.py
@@ -8,3 +8,8 @@ from . import __version__
 @click.version_option(version=__version__)
 def main() -> None:
     """Cookiecutter Hypermodern Python Instance."""
+
+
+def this_function_has_no_coverage(n: int) -> int:
+    """This function has no coverage."""
+    return n + 1
