e55afe1bed7ea42fd46472ac157faac8cfb561c0
Claudio Jolowicz <mail@claudiojolowicz.com>
2020-04-16 13:00:02 +0200

    Remove recommonmark (#101)
    * Remove recommonmark from Sphinx configuration
    
    * Remove recommonmark
    
    * Remove recommonmark from docs/requirements.txt
    
    * Remove recommonmark from Nox session

--- docs/conf.py
+++ docs/conf.py
@@ -5,9 +5,4 @@ from datetime import datetime
 project = "Cookiecutter Hypermodern Python Instance"
 author = "Claudio Jolowicz"
 copyright = f"{datetime.now().year}, {author}"
-extensions = [
-    "recommonmark",
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
-    "sphinx_autodoc_typehints",
-]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_autodoc_typehints"]
