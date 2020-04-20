389cdda15efb0b4b6557b6f9222e8c52e8cecf3f
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-04-20 08:31:52 +0200

    Configure sphinx.ext.autodoc to include type hints in description
    http://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#confval-autodoc_typehints

--- docs/conf.py
+++ docs/conf.py
@@ -6,3 +6,4 @@ project = "Cookiecutter Hypermodern Python Instance"
 author = "Claudio Jolowicz"
 copyright = f"{datetime.now().year}, {author}"
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_autodoc_typehints"]
+autodoc_typehints = "description"
