5baab73effbb86764a53b7e7daf08064da3b4e45
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-07-17 18:49:50 +0200

    Enable code highlighting with pytest
    Install pygments into the `tests` and `typeguard` Nox sessions.

--- noxfile.py
+++ noxfile.py
@@ -117,7 +117,7 @@ def mypy(session: Session) -> None:
 def tests(session: Session) -> None:
     """Run the test suite."""
     session.install(".")
-    session.install("coverage[toml]", "pytest")
+    session.install("coverage[toml]", "pytest", "pygments")
     try:
         session.run("coverage", "run", "--parallel", "-m", "pytest", *session.posargs)
     finally:
@@ -144,7 +144,7 @@ def coverage(session: Session) -> None:
 def typeguard(session: Session) -> None:
     """Runtime type checking using Typeguard."""
     session.install(".")
-    session.install("pytest", "typeguard")
+    session.install("pytest", "typeguard", "pygments")
     session.run("pytest", f"--typeguard-packages={package}", *session.posargs)
 
 
