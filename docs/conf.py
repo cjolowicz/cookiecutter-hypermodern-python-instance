473ffb963d519a63578484bd6d4af6b3a262e258
Steven van de Graaf <8904453+sgraaf@users.noreply.github.com>
2022-01-12 06:16:33 +0000

    Use author template variable in docs' copyright (#1110)

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 """Sphinx configuration."""
 project = "Cookiecutter Hypermodern Python Instance"
 author = "Claudio Jolowicz"
-copyright = "2022, {author}"
+copyright = "2022, Claudio Jolowicz"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
